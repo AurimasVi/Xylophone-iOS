94fb375f854f23c4195b6994283c356c66132dc4
AurimasVi <aurimas.vidutis@gmail.com>
2024-06-05 16:48:20 +0300

    Updated small animation

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -4,20 +4,27 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var player: AVAudioPlayer!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func keyPressed(_ sender: UIButton) {
         var noteKey: String = sender.currentTitle!
+        sender.alpha = 0.5
         playSound(noteKey: noteKey)
-    }
-    
-    func playSound(noteKey: String) {
-        let url = Bundle.main.url(forResource: noteKey, withExtension: "wav")
-        player = try! AVAudioPlayer(contentsOf: url!)
-        player.play()
-                
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2 ) {
+            sender.alpha = 1.0
+            
+            
+        }
+        
+        func playSound(noteKey: String) {
+            let url = Bundle.main.url(forResource: noteKey, withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+            
+        }
+        
     }
 }
